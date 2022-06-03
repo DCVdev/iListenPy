5f39ca0adedbd5d143d5da76f81b4950f26513af
dcardozo <dcardozo@cucorent.com>
2022-06-03 13:28:50 +0200

    añadí botones

--- caja.py
+++ caja.py
@@ -1,11 +1,8 @@
-from cgitb import text
-from email.mime import audio
 from lib2to3.pgen2.literals import evalString
 import os,sys,glob
 from tkinter import *
 from tkinter import ttk
 from PIL import Image,ImageTk
-from mutagen.mp3 import MP3
 from functools import partial
 import pygame 
 pygame.init()
@@ -19,7 +16,6 @@ y=1
 musicas=[]
 xmusic=[]
 i=0
-
 def cargarCancion(music):
     musicas.append(music)
 def playCancion(btn):
@@ -28,8 +24,6 @@ def playCancion(btn):
     pygame.mixer.music.play()
 for m in glob.glob("music/*.mp3"):
     cargarCancion(m)
-
-
 imagenes=[]
 def displayImg(img,z):
     image = Image.open(img)
@@ -42,7 +36,6 @@ def displayImg(img,z):
     photo_txt = Label(text=img)
     newPhoto_label.grid(column=posx,row=posy)
     photo_txt.grid(column=x,row=y)
-
 for f in glob.glob("imgs/*.*"):
     displayImg(f,i)
     if(i<29):
@@ -54,7 +47,14 @@ for f in glob.glob("imgs/*.*"):
         posx=0
         x=0
         y+=2
-
-
+        
+"""for img in glob.glob("")
+def cargarBotones():
+    zet = "imgbtn/play.jpg"
+    images = Image.open(zet)
+    imagez= images.resize((50,50))
+    play=ImageTk.PhotoImage(imagez)
+    playtbn=Button(image=play)
+    playtbn.grid(column=x,row=y)
+cargarBotones()"""
 root.mainloop()
-
