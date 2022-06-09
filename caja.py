d5635f6112694396a9a65ddddb38de69e0af7017
Daniel Cardozo <villa5041@gmail.com>
2022-06-09 09:40:06 +0200

    realizando cambios desde la semana pasada

--- caja.py
+++ caja.py
@@ -5,26 +5,30 @@ from tkinter import ttk
 from PIL import Image,ImageTk
 from functools import partial
 import pygame 
+#Inicio de pygame
 pygame.init()
+#Inicio del Frame
 root = Tk()
 frm = ttk.Frame(root)
 frm.grid()
+#Se crea las variables que servirán para posicionar las imágenes y botones y para guardarlas en arrays
 posx=0
 posy=0
-x =0
+x=0
 y=1
 musicas=[]
-xmusic=[]
+imgsbotons=[]
+imagenes=[]
 i=0
+#Función que carga las canciones
 def cargarCancion(music):
     musicas.append(music)
+#Función que reproduce las canciones
 def playCancion(btn):
     z = int(btn)
     pygame.mixer.music.load(musicas[z])
     pygame.mixer.music.play()
-for m in glob.glob("music/*.mp3"):
-    cargarCancion(m)
-imagenes=[]
+#Función que inserta la imagen en el frame
 def displayImg(img,z):
     image = Image.open(img)
     imagen = image.resize((100,100))
@@ -36,6 +40,65 @@ def displayImg(img,z):
     photo_txt = Label(text=img)
     newPhoto_label.grid(column=posx,row=posy)
     photo_txt.grid(column=x,row=y)
+#Función que añade el botón de reproducir
+def botonPlay(z):
+    btnplay = Image.open(z)
+    imagez= btnplay.resize((50,50))
+    play=ImageTk.PhotoImage(imagez)
+    imgsbotons.append(play)
+    playtbn=Button(image=play,command=reproducir)
+    playtbn.grid(column=x,row=y)
+#Función que añade el botón de pausa
+def botonPause(z):
+    btnpause = Image.open(z)
+    imagez= btnpause.resize((50,50))
+    pause=ImageTk.PhotoImage(imagez)
+    imgsbotons.append(pause)
+    pausebtn=Button(image=pause,command=pausar)
+    pausebtn.grid(column=x,row=y)
+#Función que añade el botón de siguiente
+def botonSiguiente(z):
+    btnsiguiente = Image.open(z)
+    imagez= btnsiguiente.resize((50,50))
+    siguiente=ImageTk.PhotoImage(imagez)
+    imgsbotons.append(siguiente)
+    siguientebtn=Button(image=siguiente,command=siguiente)
+    siguientebtn.grid(column=x,row=y)
+#Función que añade el botón de anterior
+def botonAnterior(z):
+    btnanterior = Image.open(z)
+    imagez= btnanterior.resize((50,50))
+    anterior=ImageTk.PhotoImage(imagez)
+    imgsbotons.append(anterior)
+    anteriorbtn=Button(image=anterior)
+    anteriorbtn.grid(column=x,row=y)
+#Función que añade el botón de stop
+def botonStop(z):
+    btnstop = Image.open(z)
+    imagez= btnstop.resize((50,50))
+    stop=ImageTk.PhotoImage(imagez)
+    imgsbotons.append(stop)
+    stopbtn=Button(image=stop)
+    stopbtn.grid(column=x,row=y)
+#Función que añade el botón de volumen
+def botonVolumen(z):
+    btnvolumen = Image.open(z)
+    imagez= btnvolumen.resize((50,50))
+    volumen=ImageTk.PhotoImage(imagez)
+    imgsbotons.append(volumen)
+    volumenbtn=ttk.Progressbar()
+    volumenbtn.grid(column=x,row=y)
+#Función que pausa las canciones
+def pausar():
+    pygame.mixer.music.pause()
+#Función que reproduce las canciones
+def reproducir():
+    pygame.mixer.music.unpause()
+#Función que pasa a la siguiente canción
+def volumen():
+    pygame.mixer.music.get_volume()
+    
+#bucle que lee las imágenes de las canciones y llama a la función para poner las imagenes en el frame
 for f in glob.glob("imgs/*.*"):
     displayImg(f,i)
     if(i<29):
@@ -47,14 +110,27 @@ for f in glob.glob("imgs/*.*"):
         posx=0
         x=0
         y+=2
-        
-"""for img in glob.glob("")
-def cargarBotones():
-    zet = "imgbtn/play.jpg"
-    images = Image.open(zet)
-    imagez= images.resize((50,50))
-    play=ImageTk.PhotoImage(imagez)
-    playtbn=Button(image=play)
-    playtbn.grid(column=x,row=y)
-cargarBotones()"""
+#bucle que lee las canciones y llama a la función para poner las canciones en las imágenes
+for m in glob.glob("music/*.mp3"):
+        cargarCancion(m)    
+#bucle que lee los botones y tiene condicionales para saber que botón poner y luego llama a la función   
+for img in glob.glob("imgbtn/*.jpg"):   
+    if(img=="imgbtn\play.jpg"):
+        botonPlay(img)
+        x+=1
+    elif(img=="imgbtn\pause.jpg"):
+        botonPause(img)
+        x+=1
+    elif(img=="imgbtn\sefore.jpg"):
+        botonAnterior(img)
+        x+=1
+    elif(img=="imgbtn\siguiente.jpg"):
+        botonSiguiente(img)
+        x+=1
+    elif(img=="imgbtn\stop.jpg"):
+        botonStop(img)
+        x+=1
+    elif(img=="imgbtn\solumen.jpg"):
+        botonVolumen(img)
+        x+=1
 root.mainloop()
